68c924d055f067cdc0a4b08f3c112b5e0c1e9007
soyZambo <42820393+soyZambo@users.noreply.github.com>
2020-05-08 22:51:55 -0400

    Update multiple need fix.v
    need fix on States,Registers and base reg no CLK,

--- multiples need fix.v
+++ multiples need fix.v
@@ -1,394 +1,365 @@
 /* 
  * Do not change Module name 
 */
-module Multiple(output [15:0] register, output [2:0] Next_State, input [2:0] Curr_State, input [31:0]IR, input MOC);
-reg [4:0] counter= 3'd0;
-reg [2:0] Curr_State=3'd0;
-reg [2:0] Next_State=3'd0;
+module Multiple(output reg [15:0] register, output reg [9:0] Next_State, output reg [9:0]Curr_State, input  [31:0]IR, input MOC,input CLK);
 
+//out of declaration
+reg [2:0] counter;
+reg [3:0] Base_Register;
+initial counter<=2'b0;
+initial Base_Register<= IR[19:16];
  
-
-if(IR[20])
-    begin
-    case(IR[24:23])
+    always @(IR) 
+    if(IR[27:25]==3'b100)begin
+    
+if(IR[20])  begin
+    casez(IR[24:23])
     2'b00: begin //LDMDA 
-    if(Curr_State!= 3'd547 && counter != 3'd0)begin
-    Curr_State= 3'd560;
-    Next_State= 3'd561;         //use a possible repeat
+        if(counter==0) begin
+    Curr_State= 10'd560;
+    Next_State= 10'd561;         //use a possible repeat
     Curr_State=Next_State;
-    Next_State= 3'd562;
-    Curr_State=Next_State;
-    IF(MOC)begin        //wait for moc
-        Next_State= 3'd563;
+    Next_State= 10'd562;
+     Curr_State=Next_State;
+    if(MOC)begin        //wait for moc
+       Next_State= 10'd563;
         Curr_State=Next_State;
         end
         else begin
-        Curr_State=3'd562;
-        Next_State=3'd563;
+        Curr_State=10'd562;
+         Next_State=10'd563;
         end
-    
-        if(counter!=0)
-        begin
-        always(counter)
-       
-        register= register[counter];
-        counter= counter-1;
+    //countdown
+        if(counter!=0)  begin
+         register= register[counter];
+         counter= counter-1;
         end
     else begin
-          Curr_State= 3'd560;
-        Next_State= 3'd561;         //use a possible repeat
-        Curr_State=Next_State;
-        Next_State= 3'd562;
+        Curr_State= 10'd560;
+        Next_State= 10'd561;         //use a possible repeat
          Curr_State=Next_State;
-         Next_State= 3'd563;
+         Next_State= 10'd562;
+          Curr_State=Next_State;
+          Next_State= 10'd563;
           Curr_State=Next_State;
         end
     end
     end
     2'b01: begin //LDMIA
-    if(Curr_State!= 3'd547 && counter !=0)begin
-    Curr_State= 3'd544;
-    Next_State= 3'd545 ;         //use a possible repeat
-    Curr_State=Next_State;
-    Next_State= 3'd546;
-    Curr_State=Next_State;
-        IF(MOC)begin        //wait for moc
-        Next_State= 3'd547;
-        Curr_State=Next_State;
+         if(counter==0) begin
+     Curr_State= 10'd544;
+     Next_State= 10'd545 ;         //use a possible repeat
+     Curr_State=Next_State;
+     Next_State= 10'd546;
+     Curr_State=Next_State;
+        if(MOC)begin        //wait for moc
+         Next_State= 10'd547;
+         Curr_State=Next_State;
         end
         else begin
-        Curr_State=3'd546;
-        Next_State=3'd547;
+         Curr_State=10'd546;
+         Next_State=10'd547;
         end
         
-        if(counter!=0)
+        if(counter==0)
         begin
-        always(counter)
-       
-        register= register[counter];
-        counter= counter-1;
+         register= register[counter];
+         counter= counter-1;
         end
     else begin
-         Curr_State= 3'd544;
-         Next_State= 3'd545 ;         //use a possible repeat
+         Curr_State= 10'd544;
+         Next_State= 10'd545 ;         //use a possible repeat
+        Curr_State=Next_State;
+         Next_State= 10'd546;
          Curr_State=Next_State;
-         Next_State= 3'd546;
-         Curr_State=Next_State;
-         Next_State= 3'd547;
-         Curr_State=Next_State;
+          Next_State= 10'd547;
+          Curr_State=Next_State;
         end
     
     end
     end
     2'b10: begin //LDMDB 
-    if(Curr_State!= 3'd547 && counter !=0)begin
-    Curr_State= 3'd564;
-    Next_State= 3'd565;         //use a possible repeat
-    Curr_State=Next_State;
-    Next_State= 3'd566;
-    Curr_State=Next_State;
-    IF(MOC)begin        //wait for moc
-        Next_State= 3'd567;
-        Curr_State=Next_State;
+         if(counter==0) begin
+     Curr_State= 10'd564;
+     Next_State= 10'd565;         //use a possible repeat
+     Curr_State=Next_State;
+     Next_State= 10'd566;
+     Curr_State=Next_State;
+    if(MOC)begin        //wait for moc
+         Next_State= 10'd567;
+         Curr_State=Next_State;
         end
         else begin
-        Curr_State=3'd566;
-        Next_State=3'd567;
+         Curr_State=10'd566;
+         Next_State=10'd567;
         end
         
    
-        if(counter!=0)
+        if(counter==0)
         begin
-        always(counter)
-       
-        register= register[counter];
-        counter= counter-1;
+         register= register[counter];
+         counter= counter-1;
         end
     else begin
-        Curr_State= 3'd564;
-        Next_State= 3'd565;         //use a possible repeat
-        Curr_State=Next_State;
-        Next_State= 3'd566;
-        Curr_State=Next_State;
-        Next_State= 3'd567;
-        Curr_State=Next_State;
+         Curr_State= 10'd564;
+         Next_State= 10'd565;         //use a possible repeat
+         Curr_State=Next_State;
+         Next_State= 10'd566;
+         Curr_State=Next_State;
+         Next_State= 10'd567;
+         Curr_State=Next_State;
         end
     
     end
     end
     2'b11: begin // LDMIB
-    if(Curr_State!= 3'd547 && counter !=0)begin
-    Curr_State= 3'd548;
-    Next_State= 3'd549 ;         //use a possible repeat
-    Curr_State=Next_State;
-    Next_State= 3'd550;
-    Curr_State=Next_State;
-   IF(MOC)begin        //wait for moc
-        Next_State= 3'd551;
+    while(counter>0)begin
+     Curr_State= 10'd548;
+     Next_State= 10'd549 ;         //use a possible repeat
         Curr_State=Next_State;
+     Next_State= 10'd550;
+     Curr_State=Next_State;
+     if(MOC)begin        //wait for moc
+          Next_State= 10'd551;
+          Curr_State=Next_State;
         end
         else begin
-        Curr_State=3'd550;
-        Next_State=3'd551;
+               Curr_State=10'd550;
+               Next_State=10'd551;
         end
         
         if(counter!=0)
         begin
-        always(counter)
        
-        register= register[counter];
-        counter= counter-1;
+       
+         register= register[counter];
+         counter= counter-1;
         end
     else begin
-          Curr_State= 3'd548;
-           Next_State= 3'd549 ;         //use a possible repeat
-         Curr_State=Next_State;
-         Next_State= 3'd550;
-         Curr_State=Next_State;
-         Next_State= 3'd551;
+           Curr_State= 10'd548;
+          Next_State= 10'd549 ;         //use a possible repeat
           Curr_State=Next_State;
+          Next_State= 10'd550;
+          Curr_State=Next_State;
+          Next_State= 10'd551;
+           Curr_State=Next_State;
         end
     
     end
     end
+    
+    
+    endcase
+    
+  end  
 else begin
- begin
+ 
     case(IR[24:23])
     2'b00: begin //STMDA 
-     if(Curr_State!= 3'd547 && counter !=0)begin
-    Curr_State= 3'd568;
-    Next_State= 3'd569;         //use a possible repeat
-    Curr_State=Next_State;
-    Next_State= 3'd570;
-    Curr_State=Next_State;
-    Next_State= 3'd571;
-    Curr_State=Next_State;
-    IF(MOC)begin        //wait for moc
-        Next_State= 3'd1;
-        Curr_State=Next_State;
+      while(counter>0) begin
+     Curr_State= 10'd568;
+     Next_State= 10'd569;         //use a possible repeat
+     Curr_State=Next_State;
+     Next_State= 10'd570;
+     Curr_State=Next_State;
+     Next_State= 10'd571;
+     Curr_State=Next_State;
+    if(MOC)  begin        //wait for moc
+         Next_State= 10'd1;
+         Curr_State=Next_State;
         end
         else begin
-        Curr_State=3'd571;
-        Next_State=3'd1;
+         Curr_State=10'd571;
+         Next_State=10'd1;
         end
-        if(counter!=0)
-        begin
-        always(counter)
-       
-        register= register[counter];
-        counter= counter-1;
+        if(counter!=0)  begin
+         register= register[counter];
+         counter= counter-1;
         end
     else begin
-        Curr_State= 3'd568;
-        Next_State= 3'd569;         //use a possible repeat
+         Curr_State= 10'd568;
+        Next_State= 10'd569;         //use a possible repeat
         Curr_State=Next_State;
-        Next_State= 3'd570;
-        Curr_State=Next_State;
-        Next_State= 3'd571;
-        Curr_State=Next_State;
+         Next_State= 10'd570;
+         Curr_State=Next_State;
+         Next_State= 10'd571;
+         Curr_State=Next_State;
         end
     
     end
     end
     2'b01: begin //STMIA
-    if(Curr_State!= 3'd547 && counter !=0)begin
-       Curr_State= 3'd552;
-       Next_State= 3'd553 ;         //use a possible repeat
-       Curr_State=Next_State;
-        Next_State= 3'd554;
+         while(counter>0) begin
+        Curr_State= 10'd552;
+        Next_State= 10'd553 ;         //use a possible repeat
         Curr_State=Next_State;
-        Next_State= 3'd555;
-        Curr_State=Next_State;
+         Next_State= 10'd554;
+         Curr_State=Next_State;
+         Next_State= 10'd555;
+         Curr_State=Next_State;
         
-         IF(MOC)begin        //wait for moc
-        Next_State= 3'd1;
-        Curr_State=Next_State;
+         if(MOC)begin        //wait for moc
+         Next_State= 10'd1;
+         Curr_State=Next_State;
         end
         else begin
-        Curr_State=3'd555;
-        Next_State=3'd1;
+         Curr_State=10'd555;
+         Next_State=10'd1;
         end
-        
         if(counter!=0)
         begin
-        always(counter)
-       
-        register= register[counter];
-        counter= counter-1;
+         register= register[counter];
+         counter= counter-1;
         end
-    else begin
-          Curr_State= 3'd552;
-           Next_State= 3'd553 ;         //use a possible repeat
-          Curr_State=Next_State;
-          Next_State= 3'd554;
-          Curr_State=Next_State;
-          Next_State= 3'd555;
-          Curr_State=Next_State;
-        end
-    
     end
     end
     2'b10: begin //STMDB 
-    if(Curr_State!= 3'd547 && counter !=0)begin
-    Curr_State= 3'd572;
-    Next_State= 3'd573;         //use a possible repeat
-    Curr_State=Next_State;
-    Next_State= 3'd574;
-    Curr_State=Next_State;
-    Next_State= 3'd575;
-    Curr_State=Next_State;
+       while(counter>0) begin // mal, hacer que entre y repita
+        Curr_State= 10'd572;
+       Next_State= 10'd573;         //use a possible repeat
+       Curr_State=Next_State;
+       Next_State= 10'd574;
+       Curr_State=Next_State;
+       Next_State= 10'd575;
+       Curr_State=Next_State;
     
-     IF(MOC)begin        //wait for moc
-        Next_State= 3'd1;
-        Curr_State=Next_State;
+     if(MOC) begin        //wait for moc
+             Next_State= 10'd1;
+             Curr_State=Next_State; //go back to 1
         end
-        else begin
-        Curr_State=3'd575;
-        Next_State=3'd1;
+        else  begin
+               Curr_State=10'd575;
+               Next_State=10'd1;
         end
         
-        if(counter!=0)
+    if(counter!=0)
         begin
-        always(counter)
+        
        
-        register= register[counter];
-        counter= counter-1;
+         register= register[counter];
+         counter= counter-1;
         end
-    else begin
-        Curr_State= 3'd572;
-        Next_State= 3'd573;         //use a possible repeat
-        Curr_State=Next_State;
-        Next_State= 3'd574;
-        Curr_State=Next_State;
-        Next_State= 3'd575;
-        Curr_State=Next_State;
-        end
+    
     
     end
     end
     2'b11: begin // STMIB
-    if(Curr_State!= 3'd547 && counter !=0)begin
-    Curr_State= 3'd544;
-    Next_State= 3'd545 ;         //use a possible repeat
-    Curr_State=Next_State;
-    Next_State= 3'd546;
-    Curr_State=Next_State;
-    Next_State= 3'd547;
-    Curr_State=Next_State;
     
-     IF(MOC)begin        //wait for moc
-        Next_State= 3'd1;
-        Curr_State=Next_State;
-        end
+    while(counter>0)
+         begin
+       Curr_State= 10'd556;
+       Next_State= 10'd557 ;         //use a possible repeat
+       Curr_State=Next_State;
+       Next_State= 10'd558;
+       Curr_State=Next_State;
+       Next_State= 10'd559;
+       Curr_State=Next_State;
+    
+     if(MOC)begin        //wait for moc
+             Next_State= 10'd1;
+             Curr_State=Next_State;
+            end
         else begin
-        Curr_State=3'd547;
-        Next_State=3'd1;
+         Curr_State=10'd547;
+         Next_State=10'd1;
         end
         
         if(counter!=0)
         begin
-        always(counter)
-       
-        register= register[counter];
-        counter= counter-1;
+           register= register[counter];
+           counter= counter-1;
+           
         end
-    else begin
-          Curr_State= 3'd556;
-           Next_State= 3'd557;         //use a possible repeat
-         Curr_State=Next_State;
-         Next_State= 3'd558;
-         Curr_State=Next_State;
-         Next_State= 3'd559;
-          Curr_State=Next_State;
-        end
+     end
     
     end
+    
+ endcase 
+  
+    
+            
+            if(IR[0]) begin
+                    register[counter] = 4'd0;
+                    counter = counter + 1;
+                end
+
+            if(IR[1]) begin
+                    register[counter] = 4'd1;
+                    counter = counter + 1;
+
+                end
+            if(IR[2]) begin
+                   register[counter] = 4'd2;
+                   counter = counter + 1;
+                  
+                end
+            if(IR[3]) begin
+                  register[counter] = 4'd3;
+                   counter = counter + 1;
+                  
+                end
+            if(IR[4]) begin
+                  register[counter] = 4'd4;
+                   counter = counter + 1;
+                  
+                end
+            if(IR[5]) begin
+                  register[counter] = 4'd5;
+                   counter = counter + 1;
+                  
+                end
+            if(IR[6]) begin
+                  register[counter] = 4'd6;
+                   counter = counter + 1;
+                  
+                end
+            if(IR[7]) begin
+                  register[counter] = 4'd7;
+                   counter = counter + 1;
+                  
+                end
+            if(IR[8]) begin
+                  register[counter] = 4'd8;
+                   counter = counter + 1;
+                  
+                end
+            if(IR[9]) begin
+                  register[counter] = 4'd9;
+                   counter = counter + 1;
+                  
+                end
+            if(IR[10]) begin
+                  register[counter] = 4'd10;
+                   counter = counter + 1;
+                  
+                end
+            if(IR[11]) begin
+                  register[counter] = 4'd11;
+                   counter = counter + 1;
+                  
+                end
+            if(IR[12]) begin
+                  register[counter] = 4'd12;
+                   counter = counter + 1;
+                  
+                end
+            if(IR[13]) begin
+                  register[counter] = 4'd13;
+                   counter = counter + 1;
+                  
+                end
+            if(IR[14]) begin
+                  register[counter] = 4'd14;
+                   counter = counter + 1;
+                  
+                end
+            if(IR[15]) begin
+                  register[counter] = 4'd15;
+                   counter = counter + 1;
+                  
+                end
     end
-    
-end    
-    
-    
-                 if(IR[0]) begin
-                   register[counter] = 3'd0;
-                   counter = counter + 1;
-                end
-
-                 if(IR[1]) begin
-                   register[counter] = 3'd1;
-                   counter = counter + 1;
-
-                end
-                if(IR[2]) begin
-                  register[counter] = 3'd2;
-                  counter = counter + 1;
-                  
-                end
-                if(IR[3]) begin
-                 register[counter] = 3'd3;
-                  counter = counter + 1;
-                  
-                end
-                if(IR[4]) begin
-                 register[counter] = 3'd4;
-                  counter = counter + 1;
-                  
-                end
-                if(IR[5]) begin
-                 register[counter] = 3'd5;
-                  counter = counter + 1;
-                  
-                end
-                if(IR[6]) begin
-                 register[counter] = 3'd6;
-                  counter = counter + 1;
-                  
-                end
-                if(IR[7]) begin
-                 register[counter] = 3'd7;
-                  counter = counter + 1;
-                  
-                end
-                if(IR[8]) begin
-                 register[counter] = 3'd8;
-                  counter = counter + 1;
-                  
-                end
-                if(IR[9]) begin
-                 register[counter] = 3'd9;
-                  counter = counter + 1;
-                  
-                end
-                if(IR[10]) begin
-                 register[counter] = 3'd10;
-                  counter = counter + 1;
-                  
-                end
-                if(IR[11]) begin
-                 register[counter] = 3'd11;
-                  counter = counter + 1;
-                  
-                end
-                if(IR[12]) begin
-                 register[counter] = 3'd12;
-                  counter = counter + 1;
-                  
-                end
-                if(IR[13]) begin
-                 register[counter] = 3'd13;
-                  counter = counter + 1;
-                  
-                end
-                if(IR[14]) begin
-                 register[counter] = 3'd14;
-                  counter = counter + 1;
-                  
-                end
-                if(IR[15]) begin
-                 register[counter] = 3'd15;
-                  counter = counter + 1;
-                  
-                end
-end
+    end 
 endmodule
 
 //tester
@@ -396,30 +367,32 @@ endmodule
 module testMultiple;
 //inputs
 reg [31:0] IR;
-reg [9:0] Curr_State;
 reg MOC;
-
-
+reg Base_Register;
+reg CLK;
 //outputs
 wire [15:0] register;
-wire [4:0] counter=3'd0;
+wire [2:0] counter;
 wire [9:0] Next_State;
+wire [9:0] Curr_State;
 
-multiple(register,Next_State,Curr_State,IR,MOC);
+Multiple testMul(register,Next_State,Curr_State,IR,MOC,CLK);
 
 initial #50 $finish;
     initial
-        begin
-        IR=32'b0111100011001100101001000000001;
-        Curr_State=3'd1;
+        begin             //20bit
+        IR=32'b11101000101110100010000010100100;
+        MOC=1'b1;
         
-
-repeat(5) #10 IR[20]=0;
-   #10  IR[20]=1; 
+        
+repeat(5) #10 IR[20]=1'b0;
+         #10  IR[20]=1'b0; 
 end
 initial begin
-$display("IR------------ L/S---- Curr_State-----Next-State---register---counter--------Time");
-$monitor("%b---------- %b--------%d------%d------%d---%d-----------%d",IR,IR[20],Curr_State,Next_State,register,counter,$time );
+$display("IR------------ ------------ ------IR24-23----L/S----- Curr_State----Next-State------CLK---register-----Base_Reg---counter---------------Time");
+$monitor("%b----%b--------%b-------- %d ------  %d --------- %b -----%d ---------%b ------- %d - -%d",IR,IR[24:23],IR[20],Curr_State,Next_State,CLK,register,testMul.Base_Register,testMul.counter,$time );
+
+
 end
 endmodule
 
