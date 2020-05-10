da7e8d561e7016b7273e587e37d3268e835c549d
soyZambo <42820393+soyZambo@users.noreply.github.com>
2020-05-10 18:13:24 -0400

    Updated still need fix Multi
    sequential , used load 01( LDMIA) and usage of counter,

--- multiples need fix.v
+++ multiples need fix.v
@@ -8,6 +8,10 @@ reg [2:0] counter;
 reg [3:0] Base_Register;
 initial counter<=2'b0;
 initial Base_Register<= IR[19:16];
+initial Curr_State= 10'b0000000000;
+initial Next_State= 10'b0000000000;
+initial register= 15'b000000000000000;
+
  
     always @(IR) 
     if(IR[27:25]==3'b100)begin
@@ -15,101 +19,70 @@ initial Base_Register<= IR[19:16];
 if(IR[20])  begin
     casez(IR[24:23])
     2'b00: begin //LDMDA 
-        if(counter==0) begin
+       while(counter>0) begin
     Curr_State= 10'd560;
     Next_State= 10'd561;         //use a possible repeat
     Curr_State=Next_State;
     Next_State= 10'd562;
      Curr_State=Next_State;
-    if(MOC)begin        //wait for moc
-       Next_State= 10'd563;
-        Curr_State=Next_State;
+    if(MOC && Curr_State==10'd562)begin        //wait for moc
+         Next_State= 10'd563;
+         Curr_State=Next_State;
+         Base_Register= register[counter]; //add the to save in register
+         register=Base_Register;
+         Base_Register= Base_Register - 1'b1; //decrease 
+         counter= counter-1;
         end
         else begin
         Curr_State=10'd562;
          Next_State=10'd563;
         end
     //countdown
-        if(counter!=0)  begin
-         register= register[counter];
-         counter= counter-1;
-        end
-    else begin
-        Curr_State= 10'd560;
-        Next_State= 10'd561;         //use a possible repeat
-         Curr_State=Next_State;
-         Next_State= 10'd562;
-          Curr_State=Next_State;
-          Next_State= 10'd563;
-          Curr_State=Next_State;
-        end
+      
+    
     end
     end
     2'b01: begin //LDMIA
-         if(counter==0) begin
+        while(counter>0) begin
      Curr_State= 10'd544;
      Next_State= 10'd545 ;         //use a possible repeat
      Curr_State=Next_State;
      Next_State= 10'd546;
      Curr_State=Next_State;
-        if(MOC)begin        //wait for moc
+        if(MOC && Curr_State==10'd546)begin        //wait for moc
          Next_State= 10'd547;
          Curr_State=Next_State;
-        end
-        else begin
-         Curr_State=10'd546;
-         Next_State=10'd547;
-        end
-        
-        if(counter==0)
-        begin
-         register= register[counter];
+         Base_Register= register[counter]; //add the to save in register
+         register=Base_Register;
+         Base_Register= Base_Register + 1'b1; //incrase af
          counter= counter-1;
         end
-    else begin
-         Curr_State= 10'd544;
-         Next_State= 10'd545 ;         //use a possible repeat
-        Curr_State=Next_State;
-         Next_State= 10'd546;
-         Curr_State=Next_State;
-          Next_State= 10'd547;
-          Curr_State=Next_State;
+        else begin
+         Curr_State=10'd546;   //wait until moc is 1
+         Next_State=10'd547;
         end
-    
+  
     end
     end
     2'b10: begin //LDMDB 
-         if(counter==0) begin
+          while(counter>0) begin
      Curr_State= 10'd564;
      Next_State= 10'd565;         //use a possible repeat
      Curr_State=Next_State;
      Next_State= 10'd566;
      Curr_State=Next_State;
-    if(MOC)begin        //wait for moc
+    if(MOC && Curr_State==10'd566)begin        //wait for moc
          Next_State= 10'd567;
          Curr_State=Next_State;
+         Base_Register= Base_Register - 1'b1; //decrease bef
+         Base_Register= register[counter]; //add the to save in register
+         register=Base_Register;
+         counter= counter-1;
         end
         else begin
          Curr_State=10'd566;
          Next_State=10'd567;
         end
-        
-   
-        if(counter==0)
-        begin
-         register= register[counter];
-         counter= counter-1;
-        end
-    else begin
-         Curr_State= 10'd564;
-         Next_State= 10'd565;         //use a possible repeat
-         Curr_State=Next_State;
-         Next_State= 10'd566;
-         Curr_State=Next_State;
-         Next_State= 10'd567;
-         Curr_State=Next_State;
-        end
-    
     end
     end
     2'b11: begin // LDMIB
@@ -119,31 +92,20 @@ if(IR[20])  begin
         Curr_State=Next_State;
      Next_State= 10'd550;
      Curr_State=Next_State;
-     if(MOC)begin        //wait for moc
-          Next_State= 10'd551;
-          Curr_State=Next_State;
+     if(MOC && Curr_State==10'd550)begin        //wait for moc
+         Next_State= 10'd551;
+         Curr_State=Next_State;
+         Base_Register= Base_Register + 1'b1; //increase 
+         Base_Register= register[counter]; //add the to save in register
+         register=Base_Register;
+         counter= counter-1;
         end
         else begin
                Curr_State=10'd550;
                Next_State=10'd551;
         end
         
-        if(counter!=0)
-        begin
-       
-       
-         register= register[counter];
-         counter= counter-1;
-        end
-    else begin
-           Curr_State= 10'd548;
-          Next_State= 10'd549 ;         //use a possible repeat
-          Curr_State=Next_State;
-          Next_State= 10'd550;
-          Curr_State=Next_State;
-          Next_State= 10'd551;
-           Curr_State=Next_State;
-        end
+    
     
     end
     end
@@ -154,7 +116,7 @@ if(IR[20])  begin
   end  
 else begin
  
-    case(IR[24:23])
+    casez(IR[24:23])
     2'b00: begin //STMDA 
       while(counter>0) begin
      Curr_State= 10'd568;
@@ -164,28 +126,20 @@ else begin
      Curr_State=Next_State;
      Next_State= 10'd571;
      Curr_State=Next_State;
-    if(MOC)  begin        //wait for moc
-         Next_State= 10'd1;
+    if(MOC && Curr_State==10'd571)  begin        //wait for moc
+         Next_State= 10'd568;
          Curr_State=Next_State;
+         Base_Register= register[counter]; //add the to save in register
+         register=Base_Register;
+         Base_Register= Base_Register - 1'b1; //decrease af
+         counter= counter-1;
         end
         else begin
          Curr_State=10'd571;
-         Next_State=10'd1;
+         Next_State=10'd1; //possibly
         end
-        if(counter!=0)  begin
-         register= register[counter];
-         counter= counter-1;
-        end
-    else begin
-         Curr_State= 10'd568;
-        Next_State= 10'd569;         //use a possible repeat
-        Curr_State=Next_State;
-         Next_State= 10'd570;
-         Curr_State=Next_State;
-         Next_State= 10'd571;
-         Curr_State=Next_State;
-        end
-    
+        
+  
     end
     end
     2'b01: begin //STMIA
@@ -198,24 +152,24 @@ else begin
          Next_State= 10'd555;
          Curr_State=Next_State;
         
-         if(MOC)begin        //wait for moc
+         if(MOC&& Curr_State==10'd555)begin        //wait for moc
          Next_State= 10'd1;
          Curr_State=Next_State;
+         Base_Register= register[counter]; //add the to save in register
+         register=Base_Register;
+         Base_Register= Base_Register + 1'b1; //increase after 
+         counter= counter-1;
         end
         else begin
          Curr_State=10'd555;
          Next_State=10'd1;
         end
-        if(counter!=0)
-        begin
-         register= register[counter];
-         counter= counter-1;
-        end
+        
     end
     end
     2'b10: begin //STMDB 
        while(counter>0) begin // mal, hacer que entre y repita
-        Curr_State= 10'd572;
+       Curr_State= 10'd572;
        Next_State= 10'd573;         //use a possible repeat
        Curr_State=Next_State;
        Next_State= 10'd574;
@@ -223,22 +177,21 @@ else begin
        Next_State= 10'd575;
        Curr_State=Next_State;
     
-     if(MOC) begin        //wait for moc
+     if(MOC && Curr_State==10'd575) begin        //wait for moc
              Next_State= 10'd1;
              Curr_State=Next_State; //go back to 1
+             Base_Register= Base_Register - 1'b1; //decrease 
+             Base_Register= register[counter]; //add the to save in register
+             register=Base_Register;
+             Base_Register= Base_Register - 1'b1; //decrease 
+             counter= counter-1;
         end
         else  begin
                Curr_State=10'd575;
-               Next_State=10'd1;
+               Next_State=10'd575;//moc is 0, needs work
         end
         
-    if(counter!=0)
-        begin
-        
-       
-         register= register[counter];
-         counter= counter-1;
-        end
+    
     
     
     end
@@ -255,27 +208,26 @@ else begin
        Next_State= 10'd559;
        Curr_State=Next_State;
     
-     if(MOC)begin        //wait for moc
+     if(MOC && Curr_State==10'd559)begin        //wait for moc
              Next_State= 10'd1;
              Curr_State=Next_State;
+             Base_Register= Base_Register + 1'b1; //increase 
+             Base_Register= register[counter]; //add the to save in register
+             register=Base_Register;
+             counter= counter-1;
             end
         else begin
          Curr_State=10'd547;
          Next_State=10'd1;
         end
         
-        if(counter!=0)
-        begin
-           register= register[counter];
-           counter= counter-1;
-           
-        end
-     end
+        
+      end
     
     end
-    
- endcase 
   
+ endcase 
+
     
             
             if(IR[0]) begin
@@ -383,9 +335,13 @@ initial #50 $finish;
         begin             //20bit
         IR=32'b11101000101110100010000010100100;
         MOC=1'b1;
+        Base_Register=IR[19:16];
+        CLK=1'b1;
         
-        
+    repeat(counter) #5         IR=32'b11101000101110100010000010100100;
 
+        
+        
 end
 initial begin
 $display("IR------------ ------------ ------IR24-23----L/S----- Curr_State----Next-State------CLK---register-----Base_Reg---counter---------------Time");
